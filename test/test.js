4d7b338ce47c1008221b4eb99b222b92dbc7a945
Noble Amarteifio <gi1275@wayne.edu>
2020-04-23 15:47:45 -0400

    add back all test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,26 @@ const app = require('../app');
 
 
 
+//Test 1(Using supertest)
+describe ('/Route search function  and header to product page', (done) =>{
+    it('Test1: Search route, routes to product landing page', ()=>{
+        request(app)
+        .get('/search')
+        .expect(202)
+        .expect(done)
+    });
+
+});
+//Test 2(Using supertest)
+describe ('/Search function is loaded with json content type', (done) =>{
+    it('Test2: Search results loaded to rendered page(product.ejs)', ()=>{
+        request(app)
+        .get('/search')
+        .expect('Content-Type',/json/)
+        .expect(done)
+    });
+
+});
 //Test 3(Using supertest)
 describe ('/profile route renders, with loaded variable of json content type', (done) =>{
     it('Test3: json object sent to rendered page (profile)', ()=>{
@@ -11,6 +31,23 @@ describe ('/profile route renders, with loaded variable of json content type', (
         .get('/profile')
         .expect('Content-Type',/json/)
         .expect(done)
+    });
+
+});
+//Test 4(Using supertest)
+describe ('/Parse and Post Json object from Request Form', (done) =>{
+    it('Test4: JSON object parsed and posted to submitForm function', ()=>{
+        request(app)
+        .get('/RequestForm')
+        .expect('Content-Type',/json/)
+        .expect(done)
+    });
+
+});
+//Test 4(Using supertest)
+describe ('Create Listening end point for web application', () =>{
+    it('Test5: Listening Port Created', ()=>{
+        assert.equal(app());
     });
 
 });
