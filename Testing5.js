7e9b2240a8ac69428be3c6848b2c1f09ec502c27
Marwan Abbas <gr7988@wayne.edu>
2020-04-23 19:46:16 -0400

    adding test for about page

---
+++ Testing5.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var app= express(),
+    bodyParser= require("body-parser"),
+    passport= require("passport"),
+    LocalStrategy= require("passport-local");
+var alert= require("alert-node");
+app.use( express.static( "Auth" ) );
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "AstronomyNerd15",
+    database: "testlogin"
+});
+//Test 5 for app.js functions:
+
+module.exports=function (){
+    app.get("/", function(req, res) {
+         res.render("about");
+         
+    });
+    return true;
+};
